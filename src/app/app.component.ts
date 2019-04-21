9dc903d2e0c7414e4eabe274003ac92be2267468
shashank-singh2017 <shashank.singh@sjsu.edu>
2019-04-21 02:52:27 -0700

    Initial app setup with bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-first-app';
+  title = 'First Angular App';
+  name = 'shashank';
 }
